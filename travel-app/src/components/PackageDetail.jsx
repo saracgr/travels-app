d68e45751563e4c3fd8edcedb2bfe80058973d1a
saracgr <sarargcarvalho@gmail.com>
2025-05-03 15:32:24 +0100

    update image loading behavior

--- travel-app/src/components/PackageDetail.jsx
+++ travel-app/src/components/PackageDetail.jsx
@@ -9,11 +9,22 @@ export default function PackageDetail(){
     const { packages } = useContext(PackageContext)
     const [packagePlace, setPackagePlace] = useState(null);
     const [weather, setWeather] = useState(null)
+    const [loadedImages, setLoadedImages] = useState(false);
+    
+      const handleImageLoad = () => { 
+        setLoadedImages(false)
+        let loadTimeOut
+        clearTimeout(loadTimeOut)  
+        loadTimeOut = setTimeout(() => {
+        setLoadedImages(true)
+        },100)
+      };
+    
 
     useEffect(() => {
       const foundPackage = packages.find(place => place.id.toString() === id);
       setPackagePlace(foundPackage); 
-
+      handleImageLoad()
     }, [id, packages]);
 
     useEffect(() => {
@@ -28,16 +39,17 @@ export default function PackageDetail(){
         }    
     }, [])
 
-    if(!packagePlace){
-        return <p>package not found yet..</p>
-    }
-
     return(
         <>
         <section className=' details-container flex justify-center items-center align-center'>
         { packagePlace && 
                 <div key={packagePlace.city} className='details-card flex items-center justify-center align-center bg-neutral-950 rounded-md gap-5 py-5 px-5'>
-                    <img className='detailImg' src={packagePlace.src}/>
+                    <img className={`detailImg transition-opacity duration-500 
+                        ${loadedImages ? 'opacity-100' : 'opacity-0'}`} 
+                        src={packagePlace.src} 
+                        onLoad={() => handleImageLoad(packagePlace.city)}
+                        alt={packagePlace.city}
+                        />
                     <div className='details'> 
                         <h1 className='text-orange-600'>{packagePlace.city}</h1>
                         <p className='text-m'>{packagePlace.description}</p>
