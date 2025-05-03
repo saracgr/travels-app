11039e4d9330e089e00f87604ec33e94d37f9c12
saracgr <sarargcarvalho@gmail.com>
2025-05-03 17:28:32 +0100

    update image loading

--- travel-app/src/components/Packages.jsx
+++ travel-app/src/components/Packages.jsx
@@ -6,11 +6,15 @@ import { FaArrowAltCircleRight } from "react-icons/fa";
 
 export default function Packages() {
   const { packages } = useContext(PackageContext);
-  const [loadedImages, setLoadedImages] = useState({});
 
-  const handleImageLoad = (id) => {
-    setLoadedImages(prev => ({ ...prev, [id]: true }));
-  };
+   const loadTimeOut = useRef(null);
+      
+        const handleImageLoad = () => { 
+          clearTimeout(loadTimeOut.current)  
+          loadTimeOut.current = setTimeout(() => {
+          setLoadedImages(true)
+          },100)
+        };
      
 return (
     <section className='packages-container '>
@@ -20,12 +24,10 @@ return (
           packages.map(place => (
             <div key={place.city} className='card flex justify-between items-start bg-neutral-950 rounded-md gap-2 py-5 px-5'>                    
                 <div className='card-details flex items-center'> 
-                   <img className={`packageImg transition-opacity duration-100 ${
-                     loadedImages[place.id] ? 'opacity-100' : 'opacity-0'
-                   }`}
+                   <img className={`packageImg transition-opacity duration-500 ${loadedImages ? 'opacity-100' : 'opacity-0'}`} 
                    alt={`${place.city}, ${place.country}`}
                    loading="lazy"
-                   onLoad={() => handleImageLoad(place.id)}
+                   onLoad={handleImageLoad}
                    src={place.src}/>
                     <p className='text-white text-m px-3'>
                         {place.city},
